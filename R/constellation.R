d7db237dec5016c3c9120ab861e19a8fcfdb4836
josesamos <jsamos@ugr.es>
2023-08-12 10:23:04 +0200

    constellation class algorithm

--- R/constellation.R
+++ R/constellation.R
@@ -45,67 +45,63 @@ constellation <- function(name = NULL, stars = NULL) {
   dimensions = vector("list", length = length(dim_freq))
   names(dimensions) <- names(dim_freq)
 
-  # generate dimensions
-  for (dn in names(dim_freq)) {
-    if (dim_freq[dn] == 1) {
-      # finding the dimension in the component stars
-      for (s in seq_along(stars)) {
-        for (d in seq_along(stars[[s]]$instance$dimensions)) {
-          dim <- stars[[s]]$instance$dimensions[d]
-          if (names(dim) == dn) {
-            # found
-            dimensions[dn] <- dim
-            break
-          }
-        }
-        if (names(dim) == dn) {
-          break
-        }
-      }
-    } else {
-      to_conform <- vector("list", length = dim_freq[dn])
-      i <- 1
-      surrogate_key <- NULL
-      attributes <- NULL
-      for (s in seq_along(stars)) {
-        for (d in seq_along(stars[[s]]$instance$dimensions)) {
-          dim <- stars[[s]]$instance$dimensions[d]
-          if (names(dim) == dn) {
-            to_conform[i] <- dim
-            i <- i + 1
-            if (is.null(surrogate_key)) {
-              surrogate_key <- dim[[1]]$surrogate_key
-              attributes <-
-                names(dim[[1]]$table)[names(dim[[1]]$table) != surrogate_key]
-            }
-            # join facts to original dimension
-            facts[names(stars[[s]]$instance$facts)][[1]]$table <-
-              dplyr::select(
-                dplyr::inner_join(facts[names(stars[[s]]$instance$facts)][[1]]$table,
-                                  dim[[1]]$table,
-                                  by = surrogate_key),-tidyselect::all_of(surrogate_key)
-              )
-            break
-          }
-        }
-        if (i > dim_freq[dn]) {
-          break
-        }
-      }
-      dimensions[[dn]] <- conform_dimensions(to_conform)
-      # join new dimension to facts
-      for (s in seq_along(stars)) {
-        if (dn %in% names(stars[[s]]$instance$dimensions)) {
-          facts[names(stars[[s]]$instance$facts)][[1]]$table <-
-            dplyr::select(
-              dplyr::inner_join(facts[names(stars[[s]]$instance$facts)][[1]]$table,
-                                dimensions[dn][[1]]$table,
-                                by = attributes),-tidyselect::all_of(attributes)
-            )
-        }
+  for (dn in names(dim_freq[dim_freq == 1])) {
+    # finding the dimension in the component stars
+    for (s in seq_along(stars)) {
+      if (dn %in% names(stars[[s]]$instance$dimensions)) {
+        dimensions[dn] <- stars[[s]]$instance$dimensions[dn]
+        break # dim_freq == 1 and found
       }
     }
   }
+  for (dn in names(dim_freq[dim_freq > 1])) {
+    to_conform <- vector("list", length = dim_freq[dn])
+    i <- 1
+    surrogate_key <- NULL
+    attributes <- NULL
+    for (s in seq_along(stars)) {
+      if (dn %in% names(stars[[s]]$instance$dimensions)) {
+        dim <- stars[[s]]$instance$dimensions[dn]
+        to_conform[i] <- dim
+        i <- i + 1
+        if (is.null(surrogate_key)) {
+          surrogate_key <- dim[[1]]$surrogate_key
+          attributes <-
+            names(dim[[1]]$table)[names(dim[[1]]$table) != surrogate_key]
+        }
+        # join facts to original dimension
+        facts[names(stars[[s]]$instance$facts)][[1]]$table <-
+          dplyr::select(
+            dplyr::inner_join(facts[names(stars[[s]]$instance$facts)][[1]]$table,
+                              dim[[1]]$table,
+                              by = surrogate_key),-tidyselect::all_of(surrogate_key)
+          )
+      }
+      if (i > dim_freq[dn]) {
+        break
+      }
+    }
+
+    dimensions[[dn]] <- conform_dimensions(to_conform)
+
+    # join new dimension to facts
+    for (s in seq_along(stars)) {
+      i <- 1
+      if (dn %in% names(stars[[s]]$instance$dimensions)) {
+        facts[names(stars[[s]]$instance$facts)][[1]]$table <-
+          dplyr::select(
+            dplyr::inner_join(facts[names(stars[[s]]$instance$facts)][[1]]$table,
+                              dimensions[dn][[1]]$table,
+                              by = attributes),-tidyselect::all_of(attributes)
+          )
+        i <- i + 1
+      }
+      if (i > dim_freq[dn]) {
+        break
+      }
+    }
+  }
+
   # reorder attributes in facts
   for (f in seq_along(facts)) {
     measures <-
@@ -113,14 +109,16 @@ constellation <- function(name = NULL, stars = NULL) {
     facts[[f]]$table <-
       dplyr::select(facts[[f]]$table, tidyselect::all_of(c(facts[[f]]$surrogate_keys, measures)))
   }
+
   rpd <- unify_rpd(rpd)
+
   c <- structure(list(
     name = name,
     facts = facts,
     dimensions = dimensions,
     rpd = rpd
   ), class = "constellation")
-  # rpd_in_constellation(c)
+  rpd_in_constellation(c)
 }
 
 #' Unify lists of dimension names if there are any in common
