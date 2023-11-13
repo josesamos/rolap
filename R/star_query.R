33b9db829126567f4cdfe7b9b29e2450c5b9816c
José Samos <jsamos@ugr.es>
2023-11-13 12:30:12 +0100

    Geoms02 (#20)
    * use geomultistar
    
    * fix as_multistar export function
    
    * Vignette geomultistar (WIP)
    
    * geomultistar vignette
    
    * export multistar object
    
    * description and news
    
    * README
    
    * query (WIP)
    
    * query (WIP)
    
    * query (WIP)
    
    * query (WIP)
    
    * remove duplicate dimension rows
    
    * star query
    
    * star query functions
    
    * star query (WIP)
    
    * test star query

---
+++ R/star_query.R
@@ -0,0 +1,497 @@
+
+#' `star_query` S3 class
+#'
+#' An empty `star_query` object is created where we can select facts and
+#' measures, dimensions, dimension attributes and filter dimension rows.
+#'
+#' @param db A `star_database` object.
+#'
+#' @return A `star_query` object.
+#'
+#' @family query functions
+#'
+#' @examples
+#'
+#' sq <- mrs_db |>
+#'   star_query()
+#'
+#' @export
+star_query <- function(db)
+  UseMethod("star_query")
+
+#' @rdname star_query
+#'
+#' @export
+star_query.star_database <- function(db) {
+  schema <- get_star_query_schema(db)
+
+  structure(list(
+    schema = schema,
+    query = list(fact = list(),
+                 dimension = list())
+  ),
+  class = "star_query")
+}
+
+
+#' Get star query schema
+#'
+#' Obtain the star database schema to perform queries.
+#'
+#' @param db A `star_database` object.
+#'
+#' @return A list of objects (facts and dimensions schemes).
+#'
+#' @keywords internal
+get_star_query_schema <- function(db) {
+  fact <- vector("list", length = length(db$facts))
+  dimension = vector("list", length = length(db$dimensions))
+  names(fact) <- names(db$facts)
+  names(dimension) <- names(db$dimensions)
+
+  for (f in names(fact)) {
+    fact[[f]]$fk <- db$facts[[f]]$surrogate_keys
+    l <- length(db$facts[[f]]$agg)
+    fact[[f]]$nrow_agg <- db$facts[[f]]$agg[l]
+    fact[[f]]$measure <- db$facts[[f]]$agg[-l]
+  }
+
+  for (d in names(dimension)) {
+    dimension[[d]]$pk <- db$dimensions[[d]]$surrogate_key
+    attribute <-
+      setdiff(names(db$dimensions[[d]]$table),
+              db$dimensions[[d]]$surrogate_key)
+    dimension[[d]]$attribute <- attribute
+    dimension[[d]]$table <- db$dimensions[[d]]$table[0, ]
+  }
+
+  list(fact = fact,
+       dimension = dimension)
+}
+
+
+#' Select fact
+#'
+#' To define the fact to be consulted, its name is indicated, optionally, a
+#' vector of names of selected measures and another of aggregation functions are
+#' also indicated.
+#'
+#' If there is only one fact table, it is the one that is considered if no name
+#' is indicated.
+#'
+#' If no measure is given, only the one corresponding to the number of aggregated
+#' rows will be included (it is always included).
+#'
+#' If no aggregation function is given, those defined for the measures are considered.
+#'
+#' @param sq A `star_query` object.
+#' @param name A string, name of the fact.
+#' @param measures A vector of measure names.
+#' @param agg_functions A vector of aggregation function names, each one for its
+#'   corresponding measure. They can be SUM, MAX or MIN.
+#'
+#' @return A `star_query` object.
+#'
+#' @family query functions
+#'
+#' @examples
+#'
+#' sq <- mrs_db |>
+#'   star_query()
+#'
+#' sq_1 <- sq |>
+#'   select_fact(
+#'     name = "mrs_age",
+#'     measures = "all_deaths",
+#'     agg_functions = "MAX"
+#'   )
+#'
+#' sq_2 <- sq |>
+#'   select_fact(name = "mrs_age",
+#'               measures = "all_deaths")
+#'
+#' sq_3 <- sq |>
+#'   select_fact(name = "mrs_age")
+#'
+#' @export
+select_fact <- function(sq, name, measures, agg_functions) {
+  UseMethod("select_fact")
+}
+
+#' @rdname select_fact
+#' @export
+select_fact.star_query <- function(sq,
+                                   name = NULL,
+                                   measures = NULL,
+                                   agg_functions = NULL) {
+  if (is.null(name)) {
+    if (length(sq$schema$fact) == 1) {
+      name <- names(sq$schema$fact)
+    } else {
+      stopifnot("The name of the fact must be indicated." = !is.null(name))
+    }
+  } else {
+    validate_names(names(sq$schema$fact), name, concept = 'fact name')
+  }
+  stopifnot("The fact had already been selected." = !(name %in% names(sq$query$fact)))
+  stopifnot("There are repeated measures" = length(measures) == length(unique(measures)))
+  if (!is.null(measures)) {
+    validate_names(names(sq$schema$fact[[name]]$measure), measures, concept = 'measure')
+  }
+  if (!is.null(agg_functions)) {
+    validate_names(c("SUM", "MAX", "MIN"),
+                   agg_functions,
+                   concept = 'aggregation function',
+                   repeated = TRUE)
+    stopifnot(
+      "Measures and aggregation functions do not correspond." = length(measures) == length(agg_functions)
+    )
+    names(agg_functions) <- measures
+  } else {
+    agg_functions <- sq$schema$fact[[name]]$measure[measures]
+  }
+  agg_functions <- c(agg_functions, sq$schema$fact[[name]]$nrow_agg)
+
+  fact_names <- names(sq$query$fact)
+  sq$query$fact <- c(sq$query$fact, list(measure = agg_functions))
+  names(sq$query$fact) <- c(fact_names, name)
+
+  sq
+}
+
+
+#' Select dimension
+#'
+#' To add a dimension in a `star_query` object, we have to define its name and a
+#' subset of the dimension attributes. If only the name of the dimension is
+#' indicated, it is considered that all its attributes should be added.
+#'
+#' @param sq A `star_query` object.
+#' @param name A string, name of the dimension.
+#' @param attributes A vector of attribute names.
+#'
+#' @return A `star_query` object.
+#'
+#' @family query functions
+#'
+#' @examples
+#'
+#' sq <- mrs_db |>
+#'   star_query() |>
+#'   select_dimension(name = "where",
+#'                   attributes = c("city", "state")) |>
+#'   select_dimension(name = "when")
+#'
+#' @export
+select_dimension <- function(sq, name, attributes) {
+  UseMethod("select_dimension")
+}
+
+#' @rdname select_dimension
+#' @export
+select_dimension.star_query <- function(sq,
+                                        name = NULL,
+                                        attributes = NULL) {
+  stopifnot("The name of the dimension must be indicated." = !is.null(name))
+  validate_names(names(sq$schema$dimension), name, concept = 'dimension name')
+  stopifnot("The dimension had already been selected." = is.null(sq$query$dimension[[name]]$attribute))
+  attributes <- validate_names(sq$schema$dimension[[name]]$attribute, attributes, concept = 'attribute')
+  sq$query$dimension[[name]]$attribute <- attributes
+  sq
+}
+
+
+#' Filter dimension
+#'
+#' Allows you to define selection conditions for dimension rows.
+#'
+#' Conditions can be defined on any attribute of the dimension (not only on
+#' attributes selected in the query for the dimension). The selection is made
+#' based on the function `dplyr::filter`. Conditions are defined in exactly the
+#' same way as in that function.
+#'
+#' @param sq A `star_query` object.
+#' @param name A string, name of the dimension.
+#' @param ... Conditions, defined in exactly the same way as in `dplyr::filter`.
+#'
+#' @return A `star_query` object.
+#'
+#' @family query functions
+#'
+#' @examples
+#'
+#' sq <- mrs_db |>
+#'   star_query() |>
+#'   filter_dimension(name = "when", week <= " 3") |>
+#'   filter_dimension(name = "where", city == "Cambridge")
+#'
+#' @export
+filter_dimension <- function(sq, name, ...) {
+  UseMethod("filter_dimension")
+}
+
+#' @rdname filter_dimension
+#' @export
+filter_dimension.star_query <- function(sq, name = NULL, ...) {
+  stopifnot("The name of the dimension must be indicated." = !is.null(name))
+  validate_names(names(sq$schema$dimension), name, concept = 'dimension name')
+  stopifnot("The dimension had already been filtered." = is.null(sq$query$dimension[[name]]$filter))
+  dplyr::filter(sq$schema$dimension[[name]]$table, ...)
+  filter <- as.character(substitute(alist(...)))
+  # checking that it is correct (inverse operation)
+  # dplyr::filter(sq$schema$dimension[[name]]$table, eval(parse(text = filter)))
+  sq$query$dimension[[name]]$filter <- filter
+  sq
+}
+
+# ------------------------------------------------------------------------------
+
+
+#' Run query
+#'
+#' Once we have selected the facts, dimensions and defined the conditions on the
+#' instances, we can execute the query to obtain the result.
+#'
+#' As an option, we can indicate if we do not want to unify the facts in the
+#' case of having the same grain.
+#'
+#' @param db A `star_database` object.
+#' @param sq A `star_query` object.
+#'
+#' @return A `star_database` object.
+#'
+#' @family query functions
+#'
+#' @examples
+#'
+#' sq <- mrs_db |>
+#'   star_query() |>
+#'   select_dimension(name = "where",
+#'                    attributes = c("city", "state")) |>
+#'   select_dimension(name = "when",
+#'                    attributes = "year") |>
+#'   select_fact(
+#'     name = "mrs_age",
+#'     measures = "all_deaths",
+#'     agg_functions = "MAX"
+#'   ) |>
+#'   select_fact(
+#'     name = "mrs_cause",
+#'     measures = c("pneumonia_and_influenza_deaths", "all_deaths")
+#'   ) |>
+#'   filter_dimension(name = "when", week <= " 3") |>
+#'   filter_dimension(name = "where", city == "Bridgeport")
+#'
+#' mrs_db_2 <- mrs_db |>
+#'   run_query(sq)
+#'
+#' @export
+run_query <- function(db, sq)
+  UseMethod("run_query")
+
+#' @rdname run_query
+#'
+#' @export
+run_query.star_database <- function(db, sq) {
+  stopifnot("At least one fact table must be selected." = length(sq$query$fact) > 0)
+  db <- apply_select_fact(db, sq)
+  db <- apply_filter_dimension(db, sq)
+  db <- apply_select_dimension(db, sq)
+  db <- remove_duplicate_dimension_rows(db)
+  db <- group_facts(db)
+
+  db$operations <- star_operation()
+  db$lookup_tables <- list()
+  db$schemas <- list()
+  db$refresh <- list()
+  db$deploy <- list()
+  db$rpd <- list()
+
+  db
+}
+
+
+#' Apply select fact
+#'
+#' Select the facts, measures and define the aggregation functions.
+#'
+#' @param db A `star_database` object.
+#' @param sq A `star_query` object.
+#'
+#' @param db A `star_database` object.
+#'
+#' @keywords internal
+apply_select_fact <- function(db, sq) {
+  names <- names(sq$query$fact)
+  db$facts <- db$facts[names]
+  for (f in names) {
+    agg <- sq$query$fact[[f]]
+    pk <- db$facts[[f]]$surrogate_keys
+    db$facts[[f]]$table <- db$facts[[f]]$table[c(pk, names(agg))]
+    db$facts[[f]]$agg <- agg
+  }
+  db
+}
+
+
+#' Apply filter dimension
+#'
+#' Select the instances of the dimensions that meet the defined conditions.
+#'
+#' @param db A `star_database` object.
+#' @param sq A `star_query` object.
+#'
+#' @param db A `star_database` object.
+#'
+#' @keywords internal
+apply_filter_dimension <- function(db, sq) {
+  names <- names(sq$query$dimension)
+  for (d in names) {
+    filter <- sq$query$dimension[[d]]$filter
+    if (!is.null(filter)) {
+      db$dimensions[[d]]$table <-
+        dplyr::filter(db$dimensions[[d]]$table, eval(parse(text = filter)))
+      # filter facts
+      for (f in names(db$facts)) {
+        if (d %in% db$facts[[f]]$dim_int_names) {
+          pk <- db$dimensions[[d]]$surrogate_key
+          db$facts[[f]]$table <-
+            dplyr::inner_join(db$facts[[f]]$table, db$dimensions[[d]]$table[, pk], by = pk)
+        }
+      }
+    }
+  }
+  db
+}
+
+
+#' Apply select dimension
+#'
+#' Select dimensions and attributes.
+#'
+#' @param db A `star_database` object.
+#' @param sq A `star_query` object.
+#'
+#' @param db A `star_database` object.
+#'
+#' @keywords internal
+apply_select_dimension <- function(db, sq) {
+  names <- names(sq$query$dimension)
+  sel_names <- NULL
+  for (d in names) {
+    attribute <- sq$query$dimension[[d]]$attribute
+    if (!is.null(attribute)) {
+      sel_names <- c(sel_names, d)
+      pk <- db$dimensions[[d]]$surrogate_key
+      db$dimensions[[d]]$table <- db$dimensions[[d]]$table[, c(pk, attribute)]
+    }
+  }
+  # delete from facts
+  all_dimensions <- names(db$dimensions)
+  rest_names <- setdiff(all_dimensions, sel_names)
+  for (d in rest_names) {
+    for (f in names(db$facts)) {
+      if (d %in% db$facts[[f]]$dim_int_names) {
+        db$facts[[f]]$dim_int_names <- setdiff(db$facts[[f]]$dim_int_names, d)
+        stopifnot("At least one dimension must be selected for each fact table." = length(db$facts[[f]]$dim_int_names) > 0)
+        pk <- db$dimensions[[d]]$surrogate_key
+        db$facts[[f]]$surrogate_keys <- setdiff(db$facts[[f]]$surrogate_keys, pk)
+        table_fields <- names(db$facts[[f]]$table)
+        table_fields <- setdiff(table_fields, pk)
+        db$facts[[f]]$table <- db$facts[[f]]$table[, table_fields]
+      }
+    }
+  }
+  db$dimensions <- db$dimensions[sel_names]
+  db
+}
+
+
+#' Remove duplicate dimension rows
+#'
+#' After selecting only a few columns of the dimensions, there may be rows with
+#' duplicate values. We eliminate duplicates and adapt facts to the new
+#' dimensions.
+#'
+#' @param db A `star_database` object.
+#'
+#' @param db A `star_database` object.
+#'
+#' @keywords internal
+remove_duplicate_dimension_rows <- function(db) {
+  # remove duplicate dimension rows
+  for (d in names(db$dimensions)) {
+    # remove duplicates and sort
+    pk <- db$dimensions[[d]]$surrogate_key
+    attributes <- names(db$dimensions[[d]]$table)
+    i <- which(attributes == pk)
+    attributes <- attributes[-i]
+    tpk <- NULL
+    for (f in names(db$facts)) {
+      if (pk %in% db$facts[[f]]$surrogate_keys) {
+        tpk <- rbind(tpk, db$facts[[f]]$table[, pk])
+      }
+    }
+    tpk <- unique(tpk)
+    db$dimensions[[d]]$table <-
+      dplyr::inner_join(db$dimensions[[d]]$table, tpk, by = pk)
+    ft <-
+      dplyr::arrange_all(unique(db$dimensions[[d]]$table[, -i]))
+    if (nrow(db$dimensions[[d]]$table) > nrow(ft)) {
+      # add surrogate primary key
+      # := variables for parameter names
+      # !! expands the expression into a string
+      ft <-
+        tibble::add_column(ft, !!pk := 1:nrow(ft), .before = 1)
+      for (f in names(db$facts)) {
+        if (pk %in% db$facts[[f]]$surrogate_keys) {
+          # join facts to original dimension
+          db$facts[[f]]$table <-
+            dplyr::select(
+              dplyr::inner_join(db$facts[[f]]$table,
+                                db$dimensions[[d]]$table,
+                                by = pk),
+              -tidyselect::all_of(pk)
+            )
+          # join new dimension to facts
+          db$facts[[f]]$table <-
+            dplyr::select(
+              dplyr::inner_join(db$facts[[f]]$table,
+                                ft,
+                                by = attributes),
+              -tidyselect::all_of(attributes)
+            )
+        }
+      }
+      db$dimensions[[d]]$table <- ft
+    }
+  }
+  db
+}
+
+
+#' Group facts
+#'
+#' Once the external keys have been possibly replaced, group the rows of facts.
+#'
+#' @param db A `star_database` object.
+#'
+#' @param db A `star_database` object.
+#'
+#' @keywords internal
+group_facts <- function(db) {
+  for (f in names(db$facts)) {
+    fk <- db$facts[[f]]$surrogate_keys
+    measures <- names(db$facts[[f]]$table)
+    measures <- setdiff(measures, fk)
+    db$facts[[f]]$table <-
+      group_by_keys(
+        table = db$facts[[f]]$table[, c(fk, measures)],
+        keys = fk,
+        measures = measures,
+        agg_functions = db$facts[[f]]$agg[measures],
+        nrow_agg = NULL
+      )
+  }
+  db
+}
---
+++ tests/testthat/test-vignette_70.R
@@ -0,0 +1,416 @@
+test_that("query", {
+  #############################################################
+
+  sq <- mrs_db |>
+    star_query()
+
+  sq_0 <- sq
+
+  ## --------------------------------------------------------------------------------------------------------
+  sq_1 <- sq |>
+    select_fact(name = "mrs_age",
+                measures = "all_deaths",
+                agg_functions = "MAX")
+
+
+  ## --------------------------------------------------------------------------------------------------------
+  sq_2 <- sq |>
+    select_fact(name = "mrs_age",
+                measures = "all_deaths")
+
+
+  ## --------------------------------------------------------------------------------------------------------
+  sq_3 <- sq |>
+    select_fact(name = "mrs_age")
+
+
+  ## --------------------------------------------------------------------------------------------------------
+  sq_4 <- sq |>
+    select_fact(name = "mrs_age",
+                measures = "all_deaths") |>
+    select_fact(name = "mrs_cause")
+
+
+  ## --------------------------------------------------------------------------------------------------------
+  sq_5 <- sq |>
+    select_dimension(name = "where",
+                     attributes = c("city", "state"))
+
+
+  ## --------------------------------------------------------------------------------------------------------
+  sq_6 <- sq |>
+    select_dimension(name = "where")
+
+
+  ## --------------------------------------------------------------------------------------------------------
+  sq_7 <- sq |>
+    filter_dimension(name = "when", week <= " 3") |>
+    filter_dimension(name = "where", city == "Bridgeport")
+
+  ## --------------------------------------------------------------------------------------------------------
+
+  sq <- star_query(mrs_db) |>
+    select_dimension(name = "where",
+                     attributes = c("region", "state")) |>
+    select_dimension(name = "when",
+                     attributes = "year") |>
+    select_fact(name = "mrs_age",
+                measures = "all_deaths") |>
+    select_fact(name = "mrs_cause",
+                measures = "all_deaths") |>
+    filter_dimension(name = "when", week <= " 3" & year >= "2010")
+
+  mrs_db_2 <- mrs_db |>
+    run_query(sq)
+
+
+
+  #############################################################
+  expect_equal({
+    sq_1$query
+  },
+  {
+    list(fact = list(mrs_age = c(
+      all_deaths = "MAX", nrow_agg = "SUM"
+    )), dimension = list())
+  })
+
+  expect_equal({
+    sq_6$query
+  },
+  {
+    list(fact = list(), dimension = list(where = list(
+      attribute = c(
+        "region",
+        "state",
+        "city",
+        "city_state",
+        "status",
+        "pop",
+        "lat",
+        "long"
+      )
+    )))
+  })
+
+  expect_equal({
+    sq
+  },
+  {
+    structure(list(
+      schema = list(
+        fact = list(
+          mrs_cause = list(
+            fk = c("when_key",
+                   "where_key"),
+            nrow_agg = c(nrow_agg = "SUM"),
+            measure = c(
+              pneumonia_and_influenza_deaths = "SUM",
+              all_deaths = "SUM"
+            )
+          ),
+          mrs_age = list(
+            fk = c("when_key", "where_key",
+                   "who_key"),
+            nrow_agg = c(nrow_agg = "SUM"),
+            measure = c(all_deaths = "SUM")
+          )
+        ),
+        dimension = list(
+          when = list(
+            pk = "when_key",
+            attribute = c("year",
+                          "week", "week_ending_date"),
+            table = structure(
+              list(
+                when_key = integer(0),
+                year = character(0),
+                week = character(0),
+                week_ending_date = character(0)
+              ),
+              row.names = integer(0),
+              class = c("tbl_df",
+                        "tbl", "data.frame")
+            )
+          ),
+          where = list(
+            pk = "where_key",
+            attribute = c(
+              "region",
+              "state",
+              "city",
+              "city_state",
+              "status",
+              "pop",
+              "lat",
+              "long"
+            ),
+            table = structure(
+              list(
+                where_key = integer(0),
+                region = character(0),
+                state = character(0),
+                city = character(0),
+                city_state = character(0),
+                status = character(0),
+                pop = character(0),
+                lat = character(0),
+                long = character(0)
+              ),
+              row.names = integer(0),
+              class = c("tbl_df",
+                        "tbl", "data.frame")
+            )
+          ),
+          who = list(
+            pk = "who_key",
+            attribute = "age",
+            table = structure(
+              list(who_key = integer(0), age = character(0)),
+              row.names = integer(0),
+              class = c("tbl_df",
+                        "tbl", "data.frame")
+            )
+          )
+        )
+      ),
+      query = list(
+        fact = list(
+          mrs_age = c(all_deaths = "SUM",
+                      nrow_agg = "SUM"),
+          mrs_cause = c(all_deaths = "SUM", nrow_agg = "SUM")
+        ),
+        dimension = list(
+          where = list(attribute = c("region", "state")),
+          when = list(
+            attribute = "year",
+            filter = c("alist", "week <= \" 3\" & year >= \"2010\"")
+          )
+        )
+      )
+    ), class = "star_query")
+  })
+
+
+
+  expect_equal({
+    mrs_db_2
+  },
+  {
+    structure(
+      list(
+        name = "mrs",
+        operations = structure(list(
+          operations = structure(
+            list(
+              operation = character(0),
+              name = character(0),
+              details = character(0),
+              details2 = character(0),
+              order = integer(0)
+            ),
+            class = "data.frame",
+            row.names = integer(0)
+          )
+        ), class = "star_operation"),
+        lookup_tables = list(),
+        schemas = list(),
+        refresh = list(),
+        deploy = list(),
+        facts = list(
+          mrs_age = structure(
+            list(
+              name = "mrs_age",
+              surrogate_keys = c("when_key", "where_key"),
+              agg = c(all_deaths = "SUM",
+                      nrow_agg = "SUM"),
+              dim_int_names = c("when", "where"),
+              table = structure(
+                list(
+                  when_key = c(1L, 1L, 2L, 2L, 2L,
+                               2L, 3L, 4L, 4L, 5L, 5L, 6L, 7L, 7L, 7L, 7L, 7L),
+                  where_key = c(
+                    2L,
+                    8L,
+                    4L,
+                    6L,
+                    9L,
+                    14L,
+                    13L,
+                    3L,
+                    15L,
+                    5L,
+                    11L,
+                    7L,
+                    1L,
+                    3L,
+                    10L,
+                    12L,
+                    14L
+                  ),
+                  all_deaths = c(
+                    1L,
+                    50L,
+                    65L,
+                    115L,
+                    165L,
+                    44L,
+                    122L,
+                    23L,
+                    72L,
+                    327L,
+                    70L,
+                    175L,
+                    20L,
+                    74L,
+                    98L,
+                    58L,
+                    14L
+                  ),
+                  nrow_agg = c(1L, 5L, 4L, 5L, 5L, 5L, 5L, 5L,
+                               5L, 5L, 5L, 5L, 5L, 10L, 5L, 5L, 5L)
+                ),
+                class = c("tbl_df",
+                          "tbl", "data.frame"),
+                row.names = c(NA, -17L)
+              )
+            ),
+            class = "fact_table"
+          ),
+          mrs_cause = structure(
+            list(
+              name = "mrs_cause",
+              surrogate_keys = c("when_key",
+                                 "where_key"),
+              agg = c(all_deaths = "SUM", nrow_agg = "SUM"),
+              dim_int_names = c("when", "where"),
+              table = structure(
+                list(
+                  when_key = c(1L, 1L, 2L, 2L, 2L, 2L, 3L, 4L, 4L,
+                               5L, 5L, 6L, 7L, 7L, 7L, 7L, 7L),
+                  where_key = c(
+                    2L,
+                    8L,
+                    4L,
+                    6L,
+                    9L,
+                    14L,
+                    13L,
+                    3L,
+                    15L,
+                    5L,
+                    11L,
+                    7L,
+                    1L,
+                    3L,
+                    10L,
+                    12L,
+                    14L
+                  ),
+                  all_deaths = c(
+                    1L,
+                    50L,
+                    65L,
+                    116L,
+                    165L,
+                    44L,
+                    122L,
+                    23L,
+                    72L,
+                    327L,
+                    70L,
+                    175L,
+                    20L,
+                    74L,
+                    98L,
+                    58L,
+                    14L
+                  ),
+                  nrow_agg = c(1L, 1L, 1L,
+                               1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 2L, 1L, 1L,
+                               1L)
+                ),
+                class = c("tbl_df", "tbl", "data.frame"),
+                row.names = c(NA, -17L)
+              )
+            ),
+            class = "fact_table"
+          )
+        ),
+        dimensions = list(
+          where = structure(
+            list(
+              name = "where",
+              surrogate_key = "where_key",
+              table = structure(
+                list(
+                  where_key = 1:15,
+                  region = c(
+                    "1",
+                    "2",
+                    "2",
+                    "3",
+                    "3",
+                    "4",
+                    "5",
+                    "5",
+                    "6",
+                    "6",
+                    "6",
+                    "8",
+                    "8",
+                    "9",
+                    "9"
+                  ),
+                  state = c(
+                    "MA",
+                    "NJ",
+                    "NY",
+                    "IN",
+                    "OH",
+                    "MO",
+                    "FL",
+                    "VA",
+                    "AL",
+                    "KY",
+                    "TN",
+                    "CO",
+                    "NM",
+                    "CA",
+                    "WA"
+                  )
+                ),
+                row.names = c(NA, -15L),
+                class = c("tbl_df",
+                          "tbl", "data.frame")
+              )
+            ),
+            class = "dimension_table"
+          ),
+          when = structure(
+            list(
+              name = "when",
+              surrogate_key = "when_key",
+              table = structure(
+                list(
+                  when_key = 1:7,
+                  year = c("2010", "2011", "2012",
+                           "2013", "2014", "2015", "2016")
+                ),
+                row.names = c(NA, -7L),
+                class = c("tbl_df", "tbl", "data.frame")
+              )
+            ),
+            class = "dimension_table"
+          )
+        ),
+        rpd = list()
+      ),
+      class = "star_database"
+    )
+  })
+
+
+
+  #############################################################
+})
