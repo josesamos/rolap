eb9216e6e597a758fdeb43e64e35ed24c39e23bf
josesamos <jsamos@ugr.es>
2023-11-14 23:37:37 +0100

    geoattribute (WIP)

--- R/geoattribute.R
+++ R/geoattribute.R
@@ -1,3 +1,34 @@
+#' Get geometry
+#'
+#' Get the geometry of a layer, as it is interpreted to define a `geolevel`
+#' object.
+#'
+#' It will only be valid if one of the three geometries is interpreted: *point*
+#' or *polygon*.
+#'
+#' @param layer A `sf` object.
+#'
+#' @return A string.
+#'
+#' @family star database geographic attributes
+#'
+#' @examples
+#'
+#' get_geometry()
+#'
+#' @export
+get_geometry <- function(layer) {
+  geo <- unique(as.character(sf::st_geometry_type(layer, by_geometry = TRUE)))
+  if (length(intersect(geo, c("CIRCULARSTRING", "CURVEPOLYGON", "MULTIPOLYGON", "TRIANGLE", "POLYGON"))) > 0) {
+    return("polygon")
+  } else if (length(intersect(geo, c("LINESTRING", "MULTILINESTRING", "CURVE", "MULTICURVE", "COMPOUNDCURVE"))) > 0) {
+    return("line")
+  } else if (length(intersect(geo, c("POINT", "MULTIPOINT"))) > 0) {
+    return("point")
+  }
+  geo
+}
+
 
 #' Define `geoattribute` of a dimension
 #'
@@ -15,7 +46,6 @@
 #'
 #' If defined from another attribute, it should have a finer granularity, to
 #' obtain the result by grouping its instances.
-#'
 #'
 #' @param db A `star_database` object.
 #' @param dimension A string, dimension name.
@@ -54,8 +84,8 @@ define_geoattribute.star_database <- function(db,
                                               by = NULL,
                                               from_attribute = NULL) {
   stopifnot("One dimension must be indicated (only one)." = length(dimension) == 1)
-  dimension <- validate_dimension_names(db, dimension)
-  attributes <- validate_dimension_attributes(db, dimension, attribute)
+  validate_dimension_names(db, dimension)
+  validate_dimension_attributes(db, dimension, attribute)
   if (is.null(db$geo[[dimension]])) {
     db$geo[[dimension]] <- list()
   }
@@ -73,10 +103,56 @@ define_geoattribute.star_database <- function(db,
   db
 }
 
-
-#' Define an attribute from a layer
+#' Get unrelated instances of a `geoattribute`
 #'
-#' Define an attribute from a layer.
+#'
+#' @param db A `star_database` object.
+#' @param dimension A string, dimension name.
+#' @param attribute A vector, attribute names.
+#' @param from_layer A `sf` object.
+#' @param by a vector of correspondence of attributes of the dimension with the
+#'   `sf` layer structure.
+#' @param from_attribute A string, attribute name.
+#'
+#' @return A `tibble`.
+#'
+#' @family star database geographic attributes
+#'
+#' @examples
+#'
+#' geo_mrs_db <- mrs_db |>
+#'   get_unrelated_instances()
+#'
+#' @export
+get_unrelated_instances <-
+  function(db,
+           dimension,
+           attribute,
+           geometry)
+    UseMethod("get_unrelated_instances")
+
+#' @rdname get_unrelated_instances
+#'
+#' @export
+get_unrelated_instances.star_database <- function(db,
+                                              dimension = NULL,
+                                              attribute = NULL,
+                                              geometry = NULL) {
+  stopifnot("One dimension must be indicated (only one)." = length(dimension) == 1)
+  validate_dimension_names(db, dimension)
+  validate_dimension_attributes(db, dimension, attribute)
+  stopifnot("gometry must be 'point' or 'polygon'." = geometry %in% c("polygon", "point"))
+  geoatt <- get_geoattribute_name(attribute)
+  stopifnot("That geometry is not defined for the attribute." = !is.null(db$geo[[dimension]][[geoatt]][[geometry]]))
+  data_lay <- sf::st_drop_geometry(db$geo[[dimension]][[geoatt]][[geometry]])
+  data_dim <- unique(db$dimensions[[dimension]]$table[, attribute])
+  out <- dplyr::setdiff(data_dim, data_lay) |>
+    dplyr::arrange(tidyselect::all_of(attribute))
+  out
+}
+
+
+#' Define geoattribute from a layer
 #'
 #' @importFrom rlang :=
 #' @param db A `star_database` object.
@@ -133,8 +209,8 @@ define_geoattribute_from_layer <- function(db,
 #' @param db A `star_database` object.
 #' @param dimension A string, dimension name.
 #' @param attribute A string, attribute name.
+#' @param geoatt A string, geoattribute name.
 #' @param from_attribute A string, attribute name.
-#' @param additional_attributes A vector, attribute names.
 #'
 #' @return A `star_database` object.
 #'
@@ -142,62 +218,37 @@ define_geoattribute_from_layer <- function(db,
 define_geoattribute_from_attribute <- function(db,
                                                dimension = NULL,
                                                attribute = NULL,
-                                               from_attribute = NULL,
-                                               additional_attributes = NULL) {
-  stopifnot("The from_attribute must be indicated." = !is.null(from_attribute))
-  validate_names(names(db$geodimension[[dimension]]), from_attribute, concept = 'from attribute')
+                                               geoatt = NULL,
+                                               from_attribute = NULL) {
+  stopifnot("from_attribute must be indicated." = !is.null(from_attribute))
+  validate_dimension_attributes(db, dimension, from_attribute)
+  from_geoatt <- get_geoattribute_name(from_attribute)
+  stopifnot("No geometry is defined for the attribute." = !is.null(db$geo[[dimension]][[from_geoatt]]))
+  geometries <- names(db$geo[[dimension]][[from_geoatt]])
 
-  geom <- db$geodimension[[dimension]][[from_attribute]]
-  stopifnot("The geom of the from attribute must be defined." = !is.null(geom))
-
-  if (attribute == sprintf("all_%s", dimension)) {
-    db$geodimension[[dimension]][[attribute]] <- as.data.frame(geom) |>
-      dplyr::mutate(!!attribute := attribute, .before = tidyselect::all_of(from_attribute)) |>
-      sf::st_as_sf() |>
+  all_attributes <- union(from_attribute, attribute)
+  data_dim <- unique(db$dimensions[[dimension]]$table[, all_attributes])
+  if ("polygon" %in% geometries) {
+    from_layer <- db$geo[[dimension]][[from_geoatt]][["polygon"]]
+    from_layer <- dplyr::inner_join(data_dim, from_layer, by = from_attribute)
+    from_layer <- sf::st_as_sf(from_layer)
+    from_layer <- from_layer[, attribute]
+    from_layer <- from_layer |>
       dplyr::group_by_at(attribute) |>
       dplyr::summarize(.groups = "drop")
-    attr(db$geodimension[[dimension]][[attribute]], 'n_instances') <- 1
-  } else {
-    names_geom <- names(geom)
-    names_geom <- names_geom[-length(names_geom)]
-    atts <- unique(c(attribute, additional_attributes))
-    layer <- geom |>
-      dplyr::left_join(db$dimension[[dimension]], by = names_geom) |>
-      dplyr::select(tidyselect::all_of(atts)) |>
-      dplyr::group_by_at(atts) |>
-      dplyr::summarize(.groups = "drop")
+    db$geo[[dimension]][[geoatt]][["polygon"]] <- from_layer
 
-    db$geodimension[[dimension]][[attribute]] <- layer
-    attr(db$geodimension[[dimension]][[attribute]], 'n_instances') <-
-      nrow(layer)
+    # obtener los centroides
+
+  } else {
+    # obtener de varios puntos uno
+  }
+  data_lay <- sf::st_drop_geometry(from_layer)
+  out <- dplyr::setdiff(data_dim, data_lay)
+  if (nrow(out) > 0) {
+    warning("Instances of the dimension remain unrelated to the layer. Check them using `get_unrelated_instances()`.")
   }
   db
-}
-
-
-#' Get geometry
-#'
-#' Get the geometry of a layer, as it is interpreted to define a `geolevel`
-#' object.
-#'
-#' It will only be valid if one of the three geometries is interpreted: *point*
-#' or *polygon*.
-#'
-#' @param layer A `sf` object.
-#'
-#' @return A string.
-#'
-#' @keywords internal
-get_geometry <- function(layer) {
-  geo <- unique(as.character(sf::st_geometry_type(layer, by_geometry = TRUE)))
-  if (length(intersect(geo, c("CIRCULARSTRING", "CURVEPOLYGON", "MULTIPOLYGON", "TRIANGLE", "POLYGON"))) > 0) {
-    return("polygon")
-  } else if (length(intersect(geo, c("LINESTRING", "MULTILINESTRING", "CURVE", "MULTICURVE", "COMPOUNDCURVE"))) > 0) {
-    return("line")
-  } else if (length(intersect(geo, c("POINT", "MULTIPOINT"))) > 0) {
-    return("point")
-  }
-  geo
 }
 
 
